ab479c20fdc8946231c03c76496d4580a436b85a
mucha-git <d.mucha@novatek.pl>
2023-04-05 15:59:11 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,7 @@ module.exports = (env) => {
             // global app config object
             config: JSON.stringify({
                 //version: 46,
+                env: env,
                 version: process.env.REACT_APP_VERSION,
                 apiUrl: process.env.REACT_APP_API_URL
                 //apiUrl: 'http://pielgrzymka.tplinkdns.com:7010'
